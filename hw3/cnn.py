86b75991e7d55a1d92e9f1c35cdac0dff87c31a5
code14 <mob5566@gmail.com>
2016-11-17 23:13:48 +0800

    Add cnn.py for supervised learning

--- hw3/cnn.py
+++ hw3/cnn.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
 	modelname = sys.argv[2]
 
 	# load the labeled data 5000*3*32*32
-	with open(data_dir+'/all_label.p', 'rb') as infile:
+	with open(data_dir+'all_label.p', 'rb') as infile:
 		all_label = pickle.load(infile)
 	all_label = np.array(all_label).reshape(5000, 3, 32, 32)
 
@@ -41,40 +41,52 @@ if __name__ == '__main__':
 	model.add(Convolution2D(96, 3, 3, border_mode='same', input_shape=(3, 32, 32)))
 	model.add(BatchNormalization())
 	model.add(LeakyReLU())
+	model.add(Dropout(0.5))
 	model.add(Convolution2D(96, 3, 3, border_mode='same'))
 	model.add(BatchNormalization())
 	model.add(LeakyReLU())
+	model.add(Dropout(0.5))
 	model.add(Convolution2D(96, 3, 3, border_mode='same'))
 	model.add(BatchNormalization())
 	model.add(LeakyReLU())
+	model.add(Dropout(0.5))
 
 	model.add(MaxPooling2D((2, 2)))
 	model.add(BatchNormalization())
+	model.add(Dropout(0.5))
 
 	model.add(Convolution2D(192, 3, 3, border_mode='same'))
 	model.add(BatchNormalization())
 	model.add(LeakyReLU())
+	model.add(Dropout(0.5))
 	model.add(Convolution2D(192, 3, 3, border_mode='same'))
 	model.add(BatchNormalization())
 	model.add(LeakyReLU())
+	model.add(Dropout(0.5))
 	model.add(Convolution2D(192, 3, 3, border_mode='same'))
 	model.add(BatchNormalization())
 	model.add(LeakyReLU())
+	model.add(Dropout(0.5))
 
 	model.add(MaxPooling2D((2, 2)))
 	model.add(BatchNormalization())
+	model.add(Dropout(0.5))
 
 	model.add(Convolution2D(192, 3, 3, border_mode='same'))
 	model.add(BatchNormalization())
 	model.add(LeakyReLU())
+	model.add(Dropout(0.5))
 	model.add(Convolution2D(192, 1, 1, border_mode='same'))
 	model.add(BatchNormalization())
 	model.add(LeakyReLU())
+	model.add(Dropout(0.5))
 	model.add(Convolution2D(10, 1, 1, border_mode='same'))
 	model.add(BatchNormalization())
 	model.add(LeakyReLU())
+	model.add(Dropout(0.5))
 
 	model.add(GlobalAveragePooling2D())
+	model.add(Dropout(0.5))
 
 	model.add(Activation('softmax'))
 
@@ -85,7 +97,7 @@ if __name__ == '__main__':
 	
 	model.fit(all_label, labels,\
 			  nb_epoch=50,\
-			  batch_size=100)
+			  batch_size=50)
 	
 	# save the model
 	model.save(modelname)
